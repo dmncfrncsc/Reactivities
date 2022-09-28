3543876ef36a8f2453a7d23039cb814aa5ff31d9
Dominic Francisco <dominic.m.francisco@gmail.com>
2022-09-28 08:56:30 +0800

    end of section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,7 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-import { v4 as uuid } from 'uuid'
+
 
 
 export default class ActivityStore {
@@ -15,53 +15,67 @@ export default class ActivityStore {
         makeAutoObservable(this)
     }
 
-    get activitiesByDate(){
-        return Array.from(this.activityRegistry.values()).sort((a, b) => 
-        Date.parse(a.date) - Date.parse(b.date));
+    get activitiesByDate() {
+        return Array.from(this.activityRegistry.values()).sort((a, b) =>
+            Date.parse(a.date) - Date.parse(b.date));
     }
 
     loadActivities = async () => {
-    
+        this.loadingInitial = true;
         try {
             const activities = await agent.Activities.list();
 
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activityRegistry.set(activity.id, activity);
+                this.setActivity(activity);
             })
             this.setLoadingInitial(false);
-
         } catch (error) {
             console.log(error);
             this.setLoadingInitial(false);
 
         }
     }
+
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id)
+        if (activity) {
+            this.selectedActivity = activity;
+            return activity;
+        } else {
+            this.loadingInitial = true;
+            try {
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(()=>{
+                    this.selectedActivity = activity;
+                })
+                
+                this.setLoadingInitial(false);
+                return activity;
+            } catch (error) {
+                console.log(error)
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id, activity);
+    }
+    private getActivity = (id: String) => {
+        return this.activityRegistry.get(id);
+    }
+
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
     }
 
-    selectActivity = (id: String) => {
-        this.selectedActivity = this.activityRegistry.get(id);
-    }
 
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    openForm = (id?: String) => {
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
-    }
 
     createActivity = async (activity: Activity) => {
         this.loading = true;
 
-        activity.id = uuid();
         try {
             await agent.Activities.create(activity)
             runInAction(() => {
@@ -109,8 +123,6 @@ export default class ActivityStore {
             runInAction(() => {
 
                 this.activityRegistry.delete(id);
-
-                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
 
                 this.loading = false;
 
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,13 +1,17 @@
 import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 
 
 export default observer(function ActivityList() {
     const {activityStore} = useStore();
+    
     const {deleteActivity, activitiesByDate, loading} = activityStore
+
     const [target, setTarget] = useState('');
+
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id:string ){
         setTarget(e.currentTarget.name)
         deleteActivity(id)
@@ -29,7 +33,7 @@ export default observer(function ActivityList() {
                             </Item.Description>
 
                             <Item.Extra>
-                                <Button onClick={() => activityStore.selectActivity(activity.id)} 
+                                <Button as={Link} to={`/activities/${activity.id}`}
                                 floated='right' content='View' color='blue' />
                                 <Button 
                                 name={activity.id}
